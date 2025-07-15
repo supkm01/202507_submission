6149afcf3cb6dbf6972cd653bda380eaa15c5104
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-15 16:56:25 +0900

    Add files via upload

---
+++ 邵旻豪/if_statement/Practice.java
@@ -0,0 +1,24 @@
+package lesson1_3.if_statement;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		//変数の設定
+		int a,b;
+		//変数の初期化
+		a=1;
+		b=2;
+		//aとb大小を比較し、大きい方の数を出力
+		if (a>b){
+			System.out.println(a+"が大きいです");
+		} else if(a<b){
+			System.out.println(b+"が大きいです");
+		} else {
+			System.out.println("2つの数は等しいです");
+		}
+
+		
+		
+	}
+
+}
