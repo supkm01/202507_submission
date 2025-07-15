f34b26fbe605fa13281692ef30bd386b972919fc
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-15 10:27:37 +0900

    Add files via upload

---
+++ 邵旻豪/Ex2.java
@@ -0,0 +1,25 @@
+package lesson1_2.variable;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// 名前を格納する変数を初期化
+		String name = "Naruto";
+		// 名前の長さを取得
+		System.out.println(name.length());
+     
+		// 「私の名前は○○です」
+		System.out.println("私の名前は"+name+"です");
+		
+		// 練習問題
+				// 変数「myName」「age」を初期化
+				// 値は自分の名前と年齢
+				
+				// コンソールに出力「私の名前は name です。年齢は age です。」
+		String myName="SHAO MINHAO";
+		long age=28;
+		
+		System.out.println("私の名前は"+ myName +" です。年齢は" + age + " です。" );
+	}
+
+}
