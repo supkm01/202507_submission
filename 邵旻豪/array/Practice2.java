ee529ae6cb817f2d462291e5239a38d6e63e8946
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-15 15:02:31 +0900

    Add files via upload

---
+++ 邵旻豪/array/Practice2.java
@@ -0,0 +1,15 @@
+package lesson1_2.array;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// 練習問題
+		// 配列変数名「menu」を用意
+		// 初期化値「"カレー", "ハンバーグ", "焼肉"」
+		String []menu= {"カレー", "ハンバーグ", "焼肉"};
+		
+		// コンソールに出力：「注文は 焼肉 でお願いします」
+		System.out.println("注文は"+menu[2]+"でお願いします");
+	}
+
+}
