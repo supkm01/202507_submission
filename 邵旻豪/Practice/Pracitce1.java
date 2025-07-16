fc48e772c7daf4409b0ab3e45294529d1cb88ee2
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-16 13:12:31 +0900

    Add files via upload

---
+++ 邵旻豪/Practice/Pracitce1.java
@@ -0,0 +1,14 @@
+package lesson1_3.Practice;
+
+public class Pracitce1 {
+
+	public static void main(String[] args) {
+		// for文を使って下記の配列の中身をすべて表示してください
+		String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+		for (String car : cars) {
+			System.out.println(car);
+		}
+
+	}
+
+}
