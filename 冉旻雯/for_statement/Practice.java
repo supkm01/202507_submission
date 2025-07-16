54165d4ad5bf4a0587da3b8397e7486436328012
Ranminwen <ran.minwen@mct-japan.co.jp>
2025-07-16 11:52:55 +0900

    Add files via upload

---
+++ 冉旻雯/for_statement/Practice.java
@@ -0,0 +1,15 @@
+package lesson1_3.for_statement;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// 
+		for (int i = 1; i < 21; i++) {
+			int total = i;
+			System.out.println(total);
+
+		}
+
+	}
+
+}
