3f9076d75efed25b400dd545ddac4edd206159ff
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-16 11:44:59 +0900

    Add files via upload

---
+++ 邵旻豪/for_statement/Practice.java
@@ -0,0 +1,17 @@
+package lesson1_3.for_statement;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		//変数の初期化
+		int sum = 0;
+		//加算
+		for (int i = 1; i < 21; i++) {
+			sum += i;
+		}
+		//出力
+		System.out.println(sum);
+
+	}
+
+}
