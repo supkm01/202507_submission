b0e887e2fab3437b8d43be961665e424a04d620f
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-18 15:41:53 +0900

    Add files via upload

---
+++ 邵旻豪/practice3/Circle.java
@@ -0,0 +1,16 @@
+package lesson2_1.practice3;
+
+public class Circle {
+	//メンバー変数
+	double radius;
+
+	//コンストラクタ
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	//面積計算メソッド
+	void area() {
+		System.out.println(radius * radius * 3.14);
+	}
+}
---
+++ 邵旻豪/practice3/Main.java
@@ -0,0 +1,15 @@
+package lesson2_1.practice3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//インスタンス
+		Circle c1 = new Circle(2.0);
+		Circle c2 = new Circle(10.0);
+		//呼び出し
+		c1.area();
+		c2.area();
+
+	}
+
+}
