078b0cc9f0df2b0f772541cb334bc65bf964729b
Ranminwen <ran.minwen@mct-japan.co.jp>
2025-07-18 15:50:05 +0900

    Add files via upload

---
+++ 冉旻雯/lesson2_1/Practice3/Circle.java
@@ -0,0 +1,32 @@
+package lesson2_1.Practice3;
+//円の面積を扱うクラス
+public class Circle {
+	// メンバ変数
+	double radius;
+
+	//コンストラクタ
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	//円の面積を計算
+	public double area(double radius) {
+		double area;
+		return area = this.radius * 3.14;
+	}
+
+
+}
+/**
+ * 	★(Circle クラスの仕様)
+	Circle クラスは円の面積を扱うクラスです。
+	次のフィールド、メソッド、コンストラクタを作成してください。
+    ・フィールド（メンバ変数）を作成する
+	  ・半径の値 radius を double 型で宣言する
+	・コンストラクタを作成する。
+	  引数：double radius
+	  処理内容：引数を使ってメンバ変数を初期化する。
+	・area 円の面積を求めるメソッドを作成する。
+	  引数：なし
+	  戻り値：double 型 円の面積
+	  処理内容：フィールドを用いて円の面積を計算する。表示はしない。
+ */
---
+++ 冉旻雯/lesson2_1/Practice3/Main.java
@@ -0,0 +1,21 @@
+package lesson2_1.Practice3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 2つインスタンスを作成する。下記を引数として渡す。
+				// 1つ目のインスタンス作成：半径 = 2.0
+		Circle radius1 = new Circle(2.0);
+		
+				// 2つ目のインスタンス作成：半径 = 10.0
+		Circle radius2 = new Circle(2.0);		
+				// 円の面積を求めた結果をコンソールに2つ表示してください。
+		radius1.radius1;
+		radius2.radius1;
+		// 実行結果
+				// 12.56
+				// 314.0
+
+	}
+
+}
