e8a778668c064cf75255c55a36859ffc0d00e1e2
wzh614401137 <wzh614401137@gmail.com>
2025-07-23 09:23:57 +0900

    Delete 王志豪/lesson2_2/extends_ex/practice4 directory

--- 王志豪/lesson2_2/extends_ex/practice4/Printable.java
+++
@@ -1,5 +0,0 @@
-package lesson2_2.extends_ex.practice4;
-
-public interface Printable {
-	void print();
-}
--- 王志豪/lesson2_2/extends_ex/practice4/Shape.java
+++
@@ -1,8 +0,0 @@
-package lesson2_2.extends_ex.practice4;
-
-public class Shape {
-	void draw() {
-		System.out.println("Drawing a shape");
-	}
-	
-}
