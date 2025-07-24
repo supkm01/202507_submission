efa34d1781a1820f857bbbec5b4c6e592137f36b
shaominhao <shao.minhao@mct-japan.co.jp>
2025-07-24 15:37:54 +0900

    Add files via upload

--- 邵旻豪/Lesson2/singleton/practice1/Main.java
+++ 邵旻豪/Lesson2/singleton/practice1/Main.java
@@ -3,9 +3,13 @@ package lesson2_3.capsule.singleton.practice1;
 public class Main {
 
 	public static void main(String[] args) {
+		//Loginクラスから唯一のインスタンスを取得 変数名をuserLoginとすること。
 		Login userLogin = Login.getLogin();
+		//ユーザー名を取得し、表示。
 		System.out.println(userLogin.getUserName());
+		//パスワードを取得し、表示。
 		System.out.println(userLogin.getPassword());
+		//ログイン認証を実行し、結果を表示。
 		userLogin.auth();
 	}
 
