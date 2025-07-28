401dd068ec7f048033947c5a6393defccd5c84dc
wzh614401137 <wzh614401137@gmail.com>
2025-07-28 15:56:48 +0900

    Add files via upload

---
+++ 王志豪/lesson3_2/practice1/Main2.java
@@ -0,0 +1,23 @@
+package lesson3_2.practice1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// Mapの宣言（HashMap）
+		// キー：Alice バリュー：Aliceの全ての情報（名前、年齢、学籍番号）
+		Map<String, Students> students = new HashMap<String, Students>();
+		// データの追加 ※キーは名前、バリューは生徒の情報
+		students.put("Alice", new Students("Alice", 19, "110120"));
+//		students.put("Alice", "age=19, Id=110120");
+//		students.put("Bob", "age=21, Id=110121");
+//		students.put("Carol", "age=20, Id=110122");
+//		students.put("Dave", "age=19, Id=110123");
+		// キー「Alice」を指定して、Aliceの生徒の情報を表示する
+		System.out.println(students.get("Alice"));
+
+	}
+
+}
