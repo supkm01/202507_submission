a2051c41ea3fea2701963919281ea9e314c8d87f
Ranminwen <ran.minwen@mct-japan.co.jp>
2025-07-29 15:28:23 +0900

    Add files via upload

--- 冉旻雯/Lesson3/lesson3_2/practice2/Main.java
+++ 冉旻雯/Lesson3/lesson3_2/practice2/Main.java
@@ -1,33 +1,33 @@
-package lesson3_2.practice2;
+package lesson3_2.stream_ex.practice2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//リストの宣言
-		List<Integer> numbers = new ArrayList<Integer>();
-		//リストに値を追加します：15, 20, 30, 45, 55
-		numbers.add(15);
-		numbers.add(20);
-		numbers.add(30);
-		numbers.add(45);
-		numbers.add(55);
-
-		//偶数の要素をすべて削除する
-		int sum = 0;
-		for (int i = 0; i < numbers.size(); i++) {
-			if (numbers.get(i) % 2 == 0) {
-				numbers.remove(i);
-			} else {
-				sum = sum + numbers.get(i);
-			}
-
-		}
-		//合計を計算して表示します
-		System.out.println(sum);
-
+		List<String> furits = new ArrayList<String>();
+		
+		//データの挿入
+		furits.add("apple");
+		furits.add("banana");
+		furits.add("cherry");
+		furits.add("date");
+		furits.add("elderberry");
+		furits.add("fig");
+		furits.add("grape");
+		
+		//結果を表示します
+		//リストから長さが5文字以上
+		//最初の文字が'e'
+		System.out.println(
+		furits.stream()
+		.filter(e -> e.length() >5)
+		.filter(e -> e.startsWith("e"))
+		.collect(Collectors.toList()));
+		
 	}
 
 }
