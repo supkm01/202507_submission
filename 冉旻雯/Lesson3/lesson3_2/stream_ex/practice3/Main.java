e772292eb3cd807e8fe303b336a13e16210dcfbe
Ranminwen <ran.minwen@mct-japan.co.jp>
2025-07-31 17:17:54 +0900

    Add files via upload

---
+++ 冉旻雯/Lesson3/lesson3_2/stream_ex/practice3/Main.java
@@ -0,0 +1,36 @@
+package lesson3_2.stream_ex.practice3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 商品リスト
+		List<Product> productList = new ArrayList<Product>();
+		//		("Laptop", "Electronics", 100000, 5)
+		productList.add(new Product("Laptop", "Electronics", 100000, 5));
+		//		("Phone", "Electronics", 80000, 10)
+		productList.add(new Product("Phone", "Electronics", 80000, 10));
+		//		("Desk", "Furniture", 30000, 15)
+		productList.add(new Product("Desk", "Furniture", 30000, 15));
+		//		("Chair", "Furniture", 15000, 20)
+		productList.add(new Product("Chair", "Furniture", 15000, 20));
+		//		("Monitor", "Electronics", 20000, 8)
+		productList.add(new Product("Monitor", "Electronics", 20000, 8));
+		//		("Cupboard", "Furniture", 40000, 12)
+		productList.add(new Product("Cupboard", "Furniture", 40000, 12));
+		//		("Headphones", "Electronics", 15000, 25)
+		productList.add(new Product("Headphones", "Electronics", 15000, 25));
+
+		//在庫数が10未満の商品をリストアップし、商品名と在庫数を表示してください。
+		System.out.println("在庫が10未満の商品:");
+		System.out.println(
+				productList.stream()
+						.filter(e -> e.getStock() < 10)
+						.collect(Collectors.toList()));
+
+	}
+
+}
---
+++ 冉旻雯/Lesson3/lesson3_2/stream_ex/practice3/Product.java
@@ -0,0 +1,61 @@
+package lesson3_2.stream_ex.practice3;
+
+public class Product {
+	//メンバ変数
+	//商品名
+	private String name;
+	//カテゴリー
+	private String category;
+	//価格、整数で管理
+	private int price;
+	//在庫数
+	private int stock;
+
+	//コンストラクタ
+	Product(String name, String category, int price, int stock) {
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.stock = stock;
+	}
+
+	//getter setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	//toStringのオーバーライド	
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", category=" + category + ", price=" + price + ", stock=" + stock + "]";
+	}
+
+}
