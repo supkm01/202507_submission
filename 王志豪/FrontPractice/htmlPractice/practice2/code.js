06abaa92bc890fcd3edc7db0269b4ba05a2c37d4
wzh614401137 <wzh614401137@gmail.com>
2025-08-06 17:35:30 +0900

    Add files via upload

---
+++ 王志豪/FrontPractice/htmlPractice/practice2/code.js
@@ -0,0 +1,12 @@
+// マウスが"button-1"要素に乗る
+document.getElementById("button-1").addEventListener("mouseover", () => {
+    document.getElementById("output").textContent ="リンゴ";
+});
+// マウスが"button-2"要素に乗る
+document.getElementById("button-2").addEventListener("mouseover", () => {
+    document.getElementById("output").textContent ="バナナ";
+});
+// マウスが"button-3"要素に乗る
+document.getElementById("button-3").addEventListener("mouseover", () => {
+    document.getElementById("output").textContent ="みかん";
+});
